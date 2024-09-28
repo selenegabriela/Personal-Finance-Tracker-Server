6b8b67899a64a65c4127dc43a6bd8a928dd5cbc9
selene_gabriela <sel_nn@hotmail.com>
2024-09-27 22:18:58 -0600

    connection DB and server

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 
+dotenv.config();
+connectDB();
+
 const app = express()
 
-const PORT = 5000
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-  });
+app.use(express.json());
+
+//app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }) 
