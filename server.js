86cf08ab51f1d804629f0264a67d194d45480187
selene_gabriela <sel_nn@hotmail.com>
2024-09-30 20:01:49 -0600

    login and register setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -8,6 +9,7 @@ connectDB();
 const app = express()
 
 app.use(express.json());
+app.use('/api', authRoutes);
 
 //app.use('/api/auth', require('./routes/authRoutes'));
 
