c422575689cb505fb3e954be035059b052edd6d3
selene_gabriela <sel_nn@hotmail.com>
2024-10-04 08:13:32 -0600

    corse

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors')
 
 dotenv.config();
 connectDB();
 
 const app = express()
-
+app.use(cors())
 app.use(express.json());
 app.use('/api', authRoutes);
 
